bab0fb7aa64087605d4f7361f4e27b5bce41d38c
Stiliyan Ivanov <stily.c@gmail.com>
2024-02-20 04:03:55 +0200

    Copy the name of the hotcue
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>

--- hotcues-to-memory-cues.py
+++ hotcues-to-memory-cues.py
@@ -11,16 +11,17 @@ root = tree.getroot()
 for track in root.findall('./COLLECTION/TRACK'):
     for position in track.findall('POSITION_MARK'):
         start = position.get('Start')
+        name = position.get('Name')
         # don't create duplicate mem cues
         if track.findall('./POSITION_MARK[@Num="-1"][@Start="' + start + '"]'):
             print('skipping: ' + track.get('Name'))
         else:
             print('processing: ' + track.get('Name'))
             child = ET.Element('POSITION_MARK')
-            child.set('Name', '')
+            child.set('Name', name)
             child.set('Type', '0')
             child.set('Num', '-1')
             child.set('Start', start)
             track.append(child)
 
-tree.write('output.xml', encoding='UTF-8', xml_declaration=True)
+tree.write(sys.argv[2], encoding='UTF-8', xml_declaration=True)
